e9bc8ddbe2481b7a256224c2f778a86ec583bad0
JesusMartinez18 <62106052+JesusMartinez18@users.noreply.github.com>
2021-10-26 10:40:38 -0400

    Add files via upload

---
+++ Tutorials/sandbox.js
@@ -0,0 +1,42 @@
+alert('hello, world');
+console.log(1);
+console.log(2);
+let age = 25;
+let year = 2019;
+// example of comments!!!!!
+/* my name
+is jesus
+and this
+is
+a comment example */
+console.log(age, year);
+
+age= 30;
+console.log(age);
+
+
+
+var score= 75;
+console.log(score);
+
+//strings
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+// string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + '' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
